c9c45618a67c9fa6e59992826dca03ed4ed685cc
Christopher Tombleson <chris@cribznetwork.com>
2014-03-26 21:46:14 +1300

    Finished Autoloader

--- src/HackEX/Autoloader.php
+++ src/HackEX/Autoloader.php
@@ -1,63 +1,150 @@
 <?hh
+/**
+* The MIT License (MIT)
+*
+* Copyright (c) 2014 Christopher Tombleson
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+*/
+
+/**
+* HackEX\Autoloader PHP autoloader ported to Hack
+*
+* Example useage:
+*   $autoloader = new Autoloader();
+*   $autoloader->addNamespace('Test\Core', __DIR__ . '/Test/Core');
+*   $autoloader->register();
+*   $config = new Test\Core\Config();
+*
+* @author Christopher Tombleson
+*/
 namespace HackEX;
 
 class Autoloader {
-    private \string $_fileExtension = '.php';
-    private \string $_namespace;
-    private \string $_includePath;
-    private \string $_namespaceSeparator = '\\';
+    /**
+    * File extension for Hack / PHP files
+    */
+    private string $extension = ".php";
 
+    /**
+    * Namespace separator
+    */
+    private string $separator = "\\";
+
+    /**
+    * Array used to hold namespaces
+    * and their paths
+    */
+    private array<string, string> $namespaces = null;
+
+    /**
+    * __construct
+    *
+    * Create a new instance of Autoloader
+    *
+    * @param string $ns             Add a namespace when create an instance (Optional)
+    * @param string $includePath    Path to where the code is for the namespace (Optional)
+    */
     public function __construct(?\string $ns = null, ?\string $includePath = null) {
-        $this->_namespace = $ns;
-        $this->_includePath = $includePath;
+        if (!empty($ns) && !empty($includePath)) {
+            $this->addNamespace($ns, $includePath);
+        }
     }
 
-    public function setNamespaceSeparator(\string $sep): void {
-        $this->_namespaceSeparator = $sep;
+    /**
+    * addNamespace
+    *
+    * Add a namespace to the autoloader
+    *
+    * @param string $ns             Add a namespace when create an instance
+    * @param string $includePath    Path to where the code is for the namespace (Optional)
+    * @return void
+    */
+    public function addNamespace(\string $ns, \string $includePath): void {
+        if (!isset($this->namespaces[$ns])) {
+            if (file_exists($includePath)) {
+                $this->namespaces[$ns] = $includePath;
+            } else {
+                throw new AutoloaderException("Namespace include path for: " . $ns . " does not exist");
+            }
+        }
     }
 
-    public function getNamespaceSeparator(): \string {
-        return $this->_namespaceSeparator;
+    /**
+    * removeNamespace
+    *
+    * Remove a namespace from the autoloader
+    *
+    * @param string $ns     Namespace to remove from autoloader
+    * @return void
+    */
+    public function removeNamespace(\string $ns): void {
+        if (isset($this->namespaces[$ns])) {
+            unset($this->namespaces[$ns]);
+        }
     }
 
-    public function setIncludePath(\string $includePath): void {
-        $this->_includePath = $includePath;
-    }
-
-    public function getIncludePath(): \string {
-        return $this->_includePath;
-    }
-
-    public function setFileExtension(\string $fileExtension): void {
-        $this->_fileExtension = $fileExtension;
-    }
-
-    public function getFileExtension(): \string{
-        return $this->_fileExtension;
-    }
-
+    /**
+    * register
+    *
+    * Registers the autoloader function (loadClass)
+    * @return void
+    */
     public function register(): void {
         spl_autoload_register(array($this, 'loadClass'));
     }
 
+    /**
+    * unregister
+    *
+    * Unregisters the autoloader function (loadClass)
+    * @return void
+    */
     public function unregister(): void {
         spl_autoload_unregister(array($this, 'loadClass'));
     }
 
+    /**
+    * loadClass
+    *
+    * The autoloader function that will load the classes as needed
+    * @return void
+    */
     public function loadClass(\string $className): void {
-        if (null === $this->_namespace || 
-            $this->_namespace.$this->_namespaceSeparator === substr($className, 0, strlen($this->_namespace.$this->_namespaceSeparator))) {
-    
-            $fileName = '';
-            $namespace = '';
-            if (false !== ($lastNsPos = strripos($className, $this->_namespaceSeparator))) {
-                $namespace = substr($className, 0, $lastNsPos);
-                $className = substr($className, $lastNsPos + 1);
-                $fileName = str_replace($this->_namespaceSeparator, DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-            }
-            $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . $this->_fileExtension;
+        $namespace = substr($className, 0, strrpos($className, $this->separator));
+        $class = substr($className, (strrpos($className, $this->separator) + 1));
 
-            require_once ($this->_includePath !== null ? $this->_includePath . DIRECTORY_SEPARATOR : '') . $fileName;
+        echo "Namespace: " . $namespace . "\n";
+        echo "Class: " . $class . "\n";
+
+        print_r($this->namespaces);
+
+        if (!empty($namespace) && !empty($class)) {
+            if (!empty($this->namespaces[$namespace])) {
+                require_once(realpath($this->namespaces[$namespace]) . DIRECTORY_SEPARATOR . $class . $this->extension);
+            }
         }
     }
 }
+
+/**
+* Autoloader Exception
+*
+* @author Christopher Tombleson
+*/
+class AutoloaderException extends \Exception {}
